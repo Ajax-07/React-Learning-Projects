8bc04b59ca60b850f707964a9c13ff586fe3f5dc
Ajax-07 <akumar34719@gmail.com>
2023-08-12 17:39:38 +0530

    Slot project

---
+++ src/Match.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Match(props) {
+  return (
+    <>
+      <div className="slot">
+        <span>
+          {props.x} {props.y} {props.z}
+        </span>
+        <p style={{color:props.color}}>This is mathching</p>
+      </div>
+      <hr />
+    </>
+  );
+}
+
+export default Match;
